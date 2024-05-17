5341aa732e0084a34a4da3ecefc222f6ed75fb31
Jianguo Ma <jiangma@microsoft.com>
2024-05-17 16:09:22 +0800

    switch to open-telemetry agent for automatic and comprehensive instrumentation: 1. remove all micronaut and otel dependencis from the code. 2. build the docker image manually with additional otel java agent. 3. running locally with docker-compose using opentelemetry-collector-contrib
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- aca/weather-service/src/main/java/com/example/Application.java
+++ aca/weather-service/src/main/java/com/example/Application.java
@@ -1,30 +1,12 @@
 package com.example;
 
 import io.micronaut.context.annotation.Factory;
-import io.micronaut.context.event.BeanCreatedEventListener;
-import io.micronaut.jdbc.DataSourceResolver;
 import io.micronaut.runtime.Micronaut;
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.instrumentation.jdbc.datasource.OpenTelemetryDataSource;
-
-import jakarta.inject.Singleton;
-import javax.sql.DataSource;
 
 @Factory
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
-    }
-
-    @Singleton
-    BeanCreatedEventListener<DataSource> otel(OpenTelemetry telemetry, DataSourceResolver resolver) {
-        return event -> {
-            DataSource dataSource = event.getBean();
-            return new OpenTelemetryDataSource(
-                    resolver.resolve(dataSource),
-                    telemetry
-            );
-        };
     }
 }
